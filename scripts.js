a805771f9843383647d623cf5e9fddbb54daf39b
Arstan Abylov <abylovarstan7@gmail.com>
2022-02-16 14:48:22 +0600

    added 2 files

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Focus-make input active
+document.querySelector('input[type="text"]')
+    .addEventListener('focus', function() {
+        this.style.borderColor = "initial";
+    });
+// Lose focus
+document.querySelector('input[type="text"]')
+    .addEventListener('blur', function() {
+        if (!this.value) {
+            this.style.border = "red";
+        }
+    });
+
+// Blur - check if it is empty. If empty highlight borders red
+// Focus - remove red borders.
+document.querySelector('input[type="text"]')
+    .addEventListener('change', function() {
+        alert('Changed');
+    });
+
+
+// 1. Attach changed to checkbox
+// 2. If value is true output "You are emplaed" if false "You are unempled"
